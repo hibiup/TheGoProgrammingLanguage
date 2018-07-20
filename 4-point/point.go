2fe3c75f9f486a2b0daed0a242f673f2d6fda2cc
326487162 <Welcome@661>
2018-07-20 16:46:08 -0400

    Add point sample

--- 4-point/point.go
+++ 4-point/point.go
@@ -1,18 +1,10 @@
 package point_sample
 
-
-import (
-	"fmt"
-)
-
 /*
     go 语言中的指针和 C 语言非常类似
 */
-func main() {
-	x := 1            // 定义一个 int 变量 x
-	p := &x           // 指向 x 的指针。（和 C 语言类似）
-	fmt.Println(*p)   // *p 得到 p 指针指向的值
 
-	*p = 2            // 修改指针指向的内容为 2
-	fmt.Println(x)    // 也就等同修改了 x
+func TestFunc1() *int {
+	v := 1
+	return &v			// 返回局部变量地址是安全的
 }
--- 4-point/point_test.go
+++ 4-point/point_test.go
@@ -1,7 +1,31 @@
 package point_sample
 
-import "testing"
+import (
+    "testing"
+    "fmt"
+)
 
-func TestHello(t *testing.T) {
-    main()
+func TestVariablePoint(t *testing.T) {
+	x := 1				// 定义一个 int 变量 x
+	p := &x				// 指向 x 的指针。（和 C 语言类似）
+    fmt.Println(*p)		// *p 得到 p 指针指向的值
+    fmt.Println(p)
+    
+    x = 2               // 变量是 mutable 的
+    fmt.Println(*p)     // 证明该指针指向的值确实改变了。而不是像 python 那样是 immutable 的
+    fmt.Println(&x)
+
+	*p = 2				// 修改指针指向的内容为 2
+	fmt.Println(x)		// 也就等同修改了 x
+}
+
+func TestFunctionPoint(t *testing.T) {
+	p1 := TestFunc1()   // 函数可以返回指针
+    fmt.Println(p1)
+	fmt.Println(*p1)
+    
+	fmt.Println(p1 == TestFunc1())	// 但是每次可能会返回不同的值(指针)
+
+	pf := TestFunc1 		// 获得函数指针
+	fmt.Println(*pf())		// 执行函数指针，并访问返回的指针指向的值  "1"
 }
