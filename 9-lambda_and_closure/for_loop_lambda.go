b0709410714e20aa45f529bc40a8ebafc4d88cae
326487162 <Welcome@661>
2018-08-21 16:27:53 -0400

    remove empty space

--- 9-lambda_and_closure/for_loop_lambda.go
+++ 9-lambda_and_closure/for_loop_lambda.go
@@ -8,16 +8,17 @@ import (
 func for_loop_lambda() {
 	var rmdirs []func()
 	for _, d := range []string{"a", "b", "c"} {
-		fmt.Printf("x%X => %s\n", &d, d)
-		dir := d                       // NOTE:for 循环的控制体只会初始化(分配)一次控制变量。因此只有一个 d 变量。
-		os.MkdirAll(dir, 0755)         // creates parent directories too
+		fmt.Printf("x%X => %s\n", &d, d)   // NOTE:for 循环的控制体只会初始化(分配)一次控制变量。因此只有一个 d 变量。
+		dir := d                           // 匿名函数共享了调用者的栈，因此匿名函数不会为 d 入栈，
+		                                   // 因此必须分配一个新的局部变量来保存旧的值。
+		os.MkdirAll(dir, 0755)
 		rmdirs = append(rmdirs, func() {
-			os.RemoveAll(dir)
+			os.RemoveAll(dir)              // 否则匿名函数会始终得到最后一个 d 的内容。
 		})
 	}
 
 	// ...do some work…
 	for _, rmdir := range rmdirs {
-		rmdir() // clean up
+		rmdir()
 	}
 }
